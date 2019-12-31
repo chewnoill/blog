ff7126651adfd6d212c605b31b0eda211d8f2fcd
will <will@penguin>
2019-12-31 00:38:42 -0500

    [boilerplate] nextjs to parse mdx files
    Setup MDX
    ===
    
    [MDX][1] lets us combine markdown and jsx components.
    Adds some extra flavor and stlying to our content.
    
    [1]: https://github.com/mdx-js/mdx

--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
 const withCSS = require("@zeit/next-css");
+const emoji = require("remark-emoji");
+const withMDX = require("@next/mdx")({
+  options: {
+    remarkPlugins: [emoji]
+  }
+});
 
-module.exports = withCSS({});
+module.exports = withMDX(withCSS({}));
