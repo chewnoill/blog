0e1eb4528819a05f4046e599f29a7c476f1153e1
will <will@penguin>
2020-01-08 00:29:19 -0500

    [boilerplate] add some basic components
    Atomic Design
    ===
    
    So I've changed some of the names around from any
    official atomic design metaphor. The idea is to
    create a hierarchy of components where the types
    of complexity change at each layer.

--- ui/atoms.js
+++ ui/atoms.js
@@ -1,5 +1,5 @@
 import styled from "@emotion/styled";
-import { Flex } from "./base";
+import { Flex, Div, P, Pre, H3 } from "./base";
 import { divSet } from "./prop-sets";
 
 export const Page = styled(props => (
@@ -7,3 +7,33 @@ export const Page = styled(props => (
     <Flex fxd="column" maxWidth="700px" textStyle="main" {...props} />
   </Flex>
 ))(divSet);
+
+export const Card = styled(props => (
+  <Flex
+    fxd="column"
+    m="1"
+    mt="5"
+    p="3"
+    bg="white"
+    borderStyle="card"
+    {...props}
+  />
+))(divSet);
+
+export const Quote = styled(props => (
+  <Div
+    marginLeft="4"
+    paddingLeft="4"
+    borderStyle="quote"
+    textStyle="quote"
+    {...props}
+  />
+))(divSet);
+
+export const Text = styled(props => <P p="2" {...props} />)(divSet);
+
+export const Code = styled(props => (
+  <Pre textStyle="code" margin={0} {...props} />
+))(divSet);
+
+export const Label = styled(props => <H3 ff="mono" {...props} />)(divSet);
