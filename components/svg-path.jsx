e54c88c94f86e5aee9ce19b857324ee8fa14b9dd
will <will@penguin>
2020-02-11 20:23:24 -0500

    svg path follower
    okay so I came back to this change, and I didn't
    write down what it did. I don't really care about
    this feature anymore but I'm leaving it here anyway
    because I don't want to get rid of it.

--- components/svg-path.jsx
+++ components/svg-path.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { Div, divSet } from "ui";
 import styled from "@emotion/styled";
 import { keyframes } from "@emotion/core";
+import {findIndex, sum} from 'ramda';
+
 
 const followPath = keyframes`
    from {
@@ -9,17 +11,22 @@ const followPath = keyframes`
    }
    to {
      offset-distance: 100%;
-}`
+}`;
 
-const Follower = styled(Div)({
-
-    animation:`${followPath} 400s linear infinite`
-}, ({offsetPath})=>({
-    offsetPath
-}),divSet)
+const Follower = styled(Div)(
+  {
+    transition: "offset-distance 1.5s linear"
+  },
+    ({ offsetPath, offsetDistance }) => ({
+        offsetPath,
+        offsetDistance
+  }),
+  divSet
+);
 
 export const SvgLine = () => {
   const refs = useListRefs();
+    const [offsetDistance, setOffsetDistance] = React.useState(0);
 
   let counter = 0;
   const width = refs[0] && refs[0].current.offsetWidth + 110;
@@ -28,17 +35,42 @@ export const SvgLine = () => {
     counter = r.current.offsetTop;
     return ret;
   });
-  if (offsets.length === 0) {
-    return null;
-  }
 
-  var height = Math.max(
-    document.body.scrollHeight,
-    document.body.offsetHeight,
-    document.documentElement.clientHeight,
-    document.documentElement.scrollHeight,
-    document.documentElement.offsetHeight
-  );
+  const options = {
+    rootMargin: "0px",
+    threshold: 0
+  };
+  React.useEffect(() => {
+    const observer = new IntersectionObserver((intersections, obs) => {
+        console.log({ intersections, obs });
+        const visible = intersections.filter(intersection => intersection.isIntersecting)[0]
+        if(visible){
+            //find ref
+            const indexOf = findIndex(ref => ref.current === visible.target, refs);
+
+            const counters = [1,55,...offsets.slice(0,indexOf+1), width*(indexOf+1), width/-2];
+            const distance = sum(counters);
+            setOffsetDistance(distance);
+            console.log({counters},`found ${distance}`)
+        }
+    }, options);
+    refs.forEach(ref => observer.observe(ref.current));
+  }, [refs]);
+
+    const [height, setHeight] = React.useState(0);
+
+    React.useEffect(() => {
+        var height = Math.max(
+            document.body.scrollHeight,
+            document.body.offsetHeight,
+            document.documentElement.clientHeight,
+            document.documentElement.scrollHeight,
+            document.documentElement.offsetHeight
+        );
+        setHeight(height);
+    }, []);
+
+
   const path = [
     "m 1 1 l 0 55",
     ...offsets.map((offset, i) =>
@@ -55,21 +87,27 @@ export const SvgLine = () => {
   return (
     <Div pos="relative">
       <Div pos="absolute" marginTop="-15px" pointerEvents="none">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox={`0 0 ${width} ${height}`}
+        <svg xmlns="http://www.w3.org/2000/svg"
+          viewBox={`0 0 ${width || 0} ${height}`}
           height={`${height}px`}
           width={`${width}px`}
           stroke="gray"
           fill="none"
-          strokeDasharray={25000}
+          transition="stroke-dashoffset 1.5s linear"
+          strokeDashoffset={27500-offsetDistance}
+          strokeDasharray={27500}
         >
           <path d={path} />
         </svg>
       </Div>
       <Follower
-        pos="absolute" mt="-15px" bg="black" s={4} offsetPath={`path('${path}')`}
-        />
+        pos="absolute"
+        mt="-15px"
+        bg="black"
+        s={4}
+        offsetPath={`path('${path}')`}
+        offsetDistance={`${offsetDistance}px`}
+      />
     </Div>
   );
 };
